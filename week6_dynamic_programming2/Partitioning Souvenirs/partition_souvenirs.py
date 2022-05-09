8bc355f2a3b8a2fafd9bd4b048dbcdb6a3891388
BekZhoroev <69815216+BekZhoroev@users.noreply.github.com>
2022-05-09 16:12:52 -0400

    Algorithm Toolbox

---
+++ week6_dynamic_programming2/Partitioning Souvenirs/partition_souvenirs.py
@@ -0,0 +1,26 @@
+# python3
+
+
+import numpy as np
+from sys import stdin
+
+
+def partition3(values):
+    n_len = len(values)
+    if sum(values) % 3 != 0: return 0
+    sum_1_3 = sum(values)//3
+    d_values = np.zeros((n_len + 1, sum_1_3 + 1, sum_1_3 + 1))
+    d_values[0, 0, 0], d_values[:, 0, 0] = 1, 1
+    for i in range(1, n_len+1):
+        for j in range(sum_1_3+1):
+            for k in range(sum_1_3+1):
+                d_values[i, j, k] = d_values[i - 1, j, k]
+                if j >= values[i-1] and d_values[i - 1, j - values[i - 1], k]: d_values[i, j, k] = 1
+                if k >= values[i-1] and d_values[i - 1, j, k-values[i-1]]: d_values[i, j, k] = 1
+    return 1 if d_values[-1, -1, -1] == 1 else 0
+
+
+if __name__ == '__main__':
+    input_n, *input_values = list(map(int, stdin.read().split()))
+    assert input_n == len(input_values)
+    print(partition3(input_values))
